f958f9a7e8ee7a44be8b50fb4336755058544a6f
Ridhanovita <novitaridha796@gmail.com>
2025-03-13 11:29:55 +0700

    Input

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -4,3 +4,25 @@ using namespace std;
 
 int arr [20];     //Deklarasi variable global array a dengan ukuran 20
 int n;            //Deklarasi variable global n untuk menyimpan banyaknya elemen pada array
+
+void input() {    //prosedur untuk input
+    int d;
+    while (true) {
+        cout << "Masukkan banyaknya elemen pada array : "; //output ke layar
+        cin >> n;    //Input dari pengguna
+        if (n <= 20) //Jika n kurang dari atau sama dengan 20
+            break;   //Keluar dari loop
+        else {       //Jika n lebih dari 20
+            cout << "\nArray dapat mempunyai maksimal 20 elemen\n" << endl; //Output ke layar
+        }
+    }
+    cout << endl;
+    cout << "=====================" << endl;
+    cout << "Masukkan elemen array" << endl;
+    cout << "=====================" << endl;
+
+    for (int i = 0; i <n; i++){
+        cout << "Data ke-" << (i+1) <<";";
+        cin >> arr[i];
+    }
+ }
