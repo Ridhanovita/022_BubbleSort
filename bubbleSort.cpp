fcaf2597bafe471bfe1852b5b3657b01f63f6621
Ridhanovita <novitaridha796@gmail.com>
2025-03-13 11:34:38 +0700

    BubbleSortArray

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -26,3 +26,22 @@ void input() {    //prosedur untuk input
         cin >> arr[i];
     }
  }
+
+ void bubbleSortArray(){
+    int pass = 1; //step 1
+
+    do{
+        for (int j = 0; j <= n - 1 - pass; j++){ //step 2
+
+            if (arr[j] > arr[j + 1]){//step 3
+                int temp;
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        } 
+        
+        pass = pass + 1; //step 4
+
+    }while (pass <= n - 1);
+}
